f60044415f4ed4eddb5a15a10c61b32d8e6c7fea
MinimalStudios <developer.minimalstudios@gmail.com>
2022-02-14 09:29:47 +0100

    Init

---
+++ app/src/main/java/com/minimalstudios/digitstringmemory/InputDigitStringActivity.kt
@@ -0,0 +1,15 @@
+package com.minimalstudios.digitstringmemory
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.minimalstudios.digitstringmemory.databinding.ActivityInputDigitStringBinding
+
+class InputDigitStringActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityInputDigitStringBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
